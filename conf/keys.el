d92fb6a5466059cf6516c85a2932855cae7d3ad2
Dave Benjamin <dave@teamaud.io>
2026-04-09 16:40:34 -0700

    Organize keys.el into sections

--- conf/keys.el
+++ conf/keys.el
@@ -1,17 +1,21 @@
+;; Navigation
 (global-set-key (kbd "M-`") 'next-error)
-(global-set-key (kbd "<M-up>") 'enlarge-window)
-(global-set-key (kbd "<M-down>") 'shrink-window)
-
 (use-package goto-chg
   :ensure t
   :bind ("M-_" . goto-last-change))
 
+;; Scrolling
 (global-set-key (kbd "<C-S-up>") 'scroll-down-line)
 (global-set-key (kbd "<C-S-down>") 'scroll-up-line)
 
+;; Window management
+(global-set-key (kbd "<M-up>") 'enlarge-window)
+(global-set-key (kbd "<M-down>") 'shrink-window)
+(windmove-default-keybindings)
+
+;; Indentation
 (global-set-key (kbd "<M-right>") 'indent-current-region)
 (global-set-key (kbd "<M-left>") 'dedent-current-region)
 
+;; Shell
 (global-set-key (kbd "C-x m") 'shell)
-
-(windmove-default-keybindings)
