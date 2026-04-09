95eb26bdf4c5704f65f8b49a568ee0b722846ea2
Dave Benjamin <dave@teamaud.io>
2026-04-09 16:14:57 -0700

    Add scroll shortcuts

--- conf/keys.el
+++ conf/keys.el
@@ -2,6 +2,9 @@
 (global-set-key (kbd "<M-up>") 'enlarge-window)
 (global-set-key (kbd "<M-down>") 'shrink-window)
 
+(global-set-key (kbd "<C-S-up>") 'scroll-down-line)
+(global-set-key (kbd "<C-S-down>") 'scroll-up-line)
+
 (global-set-key (kbd "<M-right>") 'indent-current-region)
 (global-set-key (kbd "<M-left>") 'dedent-current-region)
 
