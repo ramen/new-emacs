1221679535f863912429460e35aaf7f59bbc1648
Dave Benjamin <dave@teamaud.io>
2026-04-09 16:47:36 -0700

    Turn off toolbar, load modus-vivendi theme

--- conf/basics.el
+++ conf/basics.el
@@ -1,7 +1,9 @@
 (menu-bar-mode -1)
+(tool-bar-mode -1)
 (setq visible-bell t)
 (setq inhibit-startup-screen t)
 (setq use-short-answers t)
+(load-theme 'modus-vivendi t)
 
 ;; Mouse support in terminal
 (unless window-system
