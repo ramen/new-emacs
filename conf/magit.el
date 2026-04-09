fcdb1dd34ea8471483f8c2d91e9c56bb15850f41
Dave Benjamin <dave@teamaud.io>
2026-04-09 16:33:34 -0700

    Add convenience magit bindings

--- conf/magit.el
+++ conf/magit.el
@@ -1,3 +1,9 @@
 (use-package magit
   :ensure t
-  :bind ("C-x g" . magit-status))
+  :bind ("C-x g" . magit-status)
+  :config
+  (transient-suffix-put 'magit-diff "r" :key "d")
+  (transient-suffix-put 'magit-fetch "u" :key "f")
+  (transient-suffix-put 'magit-pull "u" :key "F")
+  (transient-suffix-put 'magit-push "u" :key "P")
+  (transient-suffix-put 'magit-rebase "e" :key "r"))
