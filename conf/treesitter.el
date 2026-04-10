eda5d3e7e3f11bc15d94483082c5ffd6605623bb
Dave Benjamin <dave@teamaud.io>
2026-04-10 10:50:41 -0700

    Pin JavaScript grammar to ABI 14

--- conf/treesitter.el
+++ conf/treesitter.el
@@ -3,4 +3,10 @@
 (use-package treesit-auto
   :ensure t
   :custom (treesit-auto-install 'prompt)
-  :config (global-treesit-auto-mode))
+  :config
+  ;; Pin javascript grammar to last ABI-14-compatible release (Emacs 29)
+  (when (and (fboundp 'treesit-library-abi-version)
+             (eq (treesit-library-abi-version) 14))
+    (add-to-list 'treesit-language-source-alist
+                 '(javascript "https://github.com/tree-sitter/tree-sitter-javascript" "v0.23.1" "src")))
+  (global-treesit-auto-mode))
