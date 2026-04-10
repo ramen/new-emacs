70b5ec0bc45ac4798ac3e58b7533e810ebc2d991
Dave Benjamin <dave@teamaud.io>
2026-04-10 11:32:53 -0700

    Pin grammars for C/C++

--- conf/treesitter.el
+++ conf/treesitter.el
@@ -4,11 +4,15 @@
   :ensure t
   :custom (treesit-auto-install 'prompt)
   :config
-  ;; Pin grammars to last ABI-14-compatible release (Emacs 29)
+  ;; Pin grammars to last ABI-14-compatible release
   (when (and (fboundp 'treesit-library-abi-version)
              (eq (treesit-library-abi-version) 14))
     (add-to-list 'treesit-language-source-alist
                  '(javascript "https://github.com/tree-sitter/tree-sitter-javascript" "v0.23.1" "src"))
     (add-to-list 'treesit-language-source-alist
-                 '(lua "https://github.com/tree-sitter-grammars/tree-sitter-lua" "v0.3.0" "src")))
+                 '(lua "https://github.com/tree-sitter-grammars/tree-sitter-lua" "v0.3.0" "src"))
+    (add-to-list 'treesit-language-source-alist
+                 '(c "https://github.com/tree-sitter/tree-sitter-c" "v0.21.4" "src"))
+    (add-to-list 'treesit-language-source-alist
+                 '(cpp "https://github.com/tree-sitter/tree-sitter-cpp" "v0.22.3" "src")))
   (global-treesit-auto-mode))
