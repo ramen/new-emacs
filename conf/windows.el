c20bff93eeeafc5ad63144fc00f57e990950b6af
Dave Benjamin <dave@teamaud.io>
2026-04-10 06:42:48 -0700

    Enable super key on Windows

--- conf/windows.el
+++ conf/windows.el
@@ -3,4 +3,8 @@
   (setq find-ls-option '("-exec ls -ld {} \\;" . "-ld"))
   (setq find-program "wsl find")
   (setq diff-command "wsl diff")
-  (setq grep-find-template "wsl find . <X> -type f <F> -exec grep <C> -nHP -e <R> {} /dev/null \\;"))
+  (setq grep-find-template "wsl find . <X> -type f <F> -exec grep <C> -nHP -e <R> {} /dev/null \\;")
+  (setq ;; w32-pass-lwindow-to-system nil
+        ;; w32-pass-rwindow-to-system nil
+        w32-lwindow-modifier 'super
+        w32-rwindow-modifier 'super))
