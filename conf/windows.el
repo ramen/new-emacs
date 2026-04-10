38a2059bb0e61d28c86971276d47badd592cd0c7
Dave Benjamin <dave@teamaud.io>
2026-04-10 12:39:58 -0700

    Clean up frame title on Windows

--- conf/windows.el
+++ conf/windows.el
@@ -1,10 +1,13 @@
 ;;; windows.el --- Windows-specific configuration
 
 (when (eq system-type 'windows-nt)
+  (setq frame-title-format "%b")
+
+  (setq diff-command "wsl diff")
   (setq find-ls-option '("-exec ls -ld {} \\;" . "-ld"))
   (setq find-program "wsl find")
-  (setq diff-command "wsl diff")
   (setq grep-find-template "wsl find . <X> -type f <F> -exec grep <C> -nHP -e <R> {} /dev/null \\;")
+
   (setq ;; w32-pass-lwindow-to-system nil
         ;; w32-pass-rwindow-to-system nil
         w32-lwindow-modifier 'super
