b39cd648246317ffab8e8950e32b44c681b07532
Dave Benjamin <dave@teamaud.io>
2026-04-11 08:08:08 -0700

    Don't indent namespace bodies

--- conf/c.el
+++ conf/c.el
@@ -1,5 +1,17 @@
 ;;; c.el --- C/C++ configuration (tree-sitter)
 
+(defun db/c-ts-no-namespace-indent ()
+  "Zero out indentation inside C++ namespace bodies."
+  (setq-local treesit-simple-indent-rules
+              (mapcar
+               (lambda (entry)
+                 (if (eq (car entry) 'cpp)
+                     `(cpp . (((n-p-gp nil "declaration_list" "namespace_definition")
+                               grand-parent 0)
+                              ,@(cdr entry)))
+                   entry))
+               treesit-simple-indent-rules)))
+
 (use-package c-ts-mode
   :custom
   (c-ts-mode-indent-style 'bsd)
@@ -7,6 +19,7 @@
   ((c-ts-mode c++-ts-mode) . (lambda ()
                                (setq-local comment-start "// ")
                                (setq-local comment-end "")
-                               (setq-local comment-start-skip "// *"))))
+                               (setq-local comment-start-skip "// *")))
+  (c++-ts-mode . db/c-ts-no-namespace-indent))
 
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-ts-mode))
