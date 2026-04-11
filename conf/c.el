73a542a3f91a09ecb1159616594ca9db6aa90f6a
Dave Benjamin <dave@teamaud.io>
2026-04-11 08:16:36 -0700

    Don't indent preprocessor directives

--- conf/c.el
+++ conf/c.el
@@ -5,28 +5,21 @@
   (setq-local comment-start "// ")
   (setq-local comment-end "")
   (setq-local comment-start-skip "// *")
-  ;; Indent continuation parameters one level from the function start,
-  ;; and align the closing ) with the function start.
   (let ((lang (if (eq major-mode 'c++-ts-mode) 'cpp 'c)))
     (setq-local treesit-simple-indent-rules
                 (mapcar
                  (lambda (entry)
                    (if (eq (car entry) lang)
                        `(,lang .
-                         (((n-p-gp ")" "parameter_list" nil) parent-bol 0)
+                         (;; Preprocessor directives flush left
+                          ((node-is "^preproc_") (lambda (_n _p bol) bol) 0)
+                          ;; Continuation parameters indented one level, closing ) at function column
+                          ((n-p-gp ")" "parameter_list" nil) parent-bol 0)
                           ((parent-is "parameter_list") parent-bol c-ts-mode-indent-offset)
-                          ,@(cdr entry)))
-                     entry))
-                 treesit-simple-indent-rules)))
-  ;; No extra indent inside C++ namespace bodies.
-  (when (eq major-mode 'c++-ts-mode)
-    (setq-local treesit-simple-indent-rules
-                (mapcar
-                 (lambda (entry)
-                   (if (eq (car entry) 'cpp)
-                       `(cpp .
-                         (((n-p-gp nil "declaration_list" "namespace_definition")
-                           grand-parent 0)
+                          ;; No extra indent inside C++ namespace bodies
+                          ,@(when (eq lang 'cpp)
+                              '(((n-p-gp nil "declaration_list" "namespace_definition")
+                                 grand-parent 0)))
                           ,@(cdr entry)))
                      entry))
                  treesit-simple-indent-rules))))
