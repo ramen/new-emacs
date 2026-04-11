5202cfb07aa903afd0c20ee29ff5c32b90ddb5b2
Dave Benjamin <dave@teamaud.io>
2026-04-11 14:34:48 -0700

    Tweak c-mode configuration

--- conf/c-mode.el
+++ conf/c-mode.el
@@ -3,12 +3,18 @@
 (add-hook 'c-mode-common-hook
           (lambda ()
             (c-set-style "java")
+
             (c-set-offset 'access-label '-)
+            (c-set-offset 'arglist-close 0)
+            (c-set-offset 'arglist-cont-nonempty '+)
+            (c-set-offset 'arglist-intro '+)
             (c-set-offset 'case-label '+)
             (c-set-offset 'innamespace 0)
+            (c-set-offset 'statement-case-open 0)
             (c-set-offset 'substatement-open 0)
-            (setq comment-start "// ")
+
             (setq comment-end "")
+            (setq comment-start "// ")
             (setq comment-start-skip "// *")))
 
 (add-to-list 'auto-mode-alist '("\\.h$" . c++-mode))
