63d9943418e58d36e4034ea00ed80850618c3a94
Dave Benjamin <dave@teamaud.io>
2026-04-11 14:21:51 -0700

    Add an allowlist, fix C/C++ and clean up

--- conf/treesitter.el
+++ conf/treesitter.el
@@ -2,22 +2,19 @@
 
 (use-package treesit-auto
   :ensure t
-  :custom (treesit-auto-install 'prompt)
+  :custom
+  (treesit-auto-install 'prompt)
+  ;; C/C++ excluded; use c-mode/c++-mode by default (c-mode.el).
+  ;; Add c/cpp here to opt back in to tree-sitter for those languages.
+  (treesit-auto-langs '(javascript typescript tsx lua rust yaml))
   :config
   ;; Pin grammars to last ABI-14-compatible release
   (when (and (fboundp 'treesit-library-abi-version)
              (eq (treesit-library-abi-version) 14))
-    (add-to-list 'treesit-language-source-alist
-                 '(javascript "https://github.com/tree-sitter/tree-sitter-javascript" "v0.23.1" "src"))
-    (add-to-list 'treesit-language-source-alist
-                 '(lua "https://github.com/tree-sitter-grammars/tree-sitter-lua" "v0.3.0" "src"))
-    (add-to-list 'treesit-language-source-alist
-                 '(c "https://github.com/tree-sitter/tree-sitter-c" "v0.21.4" "src"))
-    (add-to-list 'treesit-language-source-alist
-                 '(cpp "https://github.com/tree-sitter/tree-sitter-cpp" "v0.21.0" "src"))
-    (add-to-list 'treesit-language-source-alist
-                 '(rust "https://github.com/tree-sitter/tree-sitter-rust" "v0.21.2" "src")))
-  (global-treesit-auto-mode)
-  ;; Keep C/C++ on classic cc-mode; tree-sitter variants still available manually
-  (dolist (mode '(c-mode c++-mode))
-    (setq major-mode-remap-alist (assq-delete-all mode major-mode-remap-alist))))
+    (dolist (entry '((c          "https://github.com/tree-sitter/tree-sitter-c"            "v0.21.4" "src")
+                     (cpp        "https://github.com/tree-sitter/tree-sitter-cpp"          "v0.21.0" "src")
+                     (javascript "https://github.com/tree-sitter/tree-sitter-javascript"   "v0.23.1" "src")
+                     (lua        "https://github.com/tree-sitter-grammars/tree-sitter-lua" "v0.3.0"  "src")
+                     (rust       "https://github.com/tree-sitter/tree-sitter-rust"         "v0.21.2" "src")))
+      (add-to-list 'treesit-language-source-alist entry)))
+  (global-treesit-auto-mode))
