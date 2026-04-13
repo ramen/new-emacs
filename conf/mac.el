285fbfb5979a2e5eb7f5f9b0fd7f9f51cb8be2ca
Dave Benjamin <dave@teamaud.io>
2026-04-13 09:53:32 -0700

    Map insert key on Mac

--- conf/mac.el
+++ conf/mac.el
@@ -2,4 +2,8 @@
 
 (when (eq system-type 'darwin)
   (setq mac-option-modifier 'super)
-  (setq mac-command-modifier 'meta))
+  (setq mac-command-modifier 'meta)
+
+  (global-set-key (kbd "<f13>") 'overwrite-mode)
+  (global-set-key (kbd "<C-f13>") 'ns-copy-including-secondary)
+  (global-set-key (kbd "<S-f13>") 'yank))
