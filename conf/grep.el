81a688dca9b74e2085f5ef96f52e430231caf994
Dave Benjamin <dave@teamaud.io>
2026-04-21 20:06:21 -0700

    Also ignore .venv and dist

--- conf/grep.el
+++ conf/grep.el
@@ -3,5 +3,7 @@
 (with-eval-after-load 'grep
   (add-hook 'grep-mode-hook (lambda () (setq-local truncate-lines t)))
 
+  (add-to-list 'grep-find-ignored-directories ".venv")
+  (add-to-list 'grep-find-ignored-directories "dist")
   (add-to-list 'grep-find-ignored-directories "node_modules")
   (add-to-list 'grep-find-ignored-directories "target"))
