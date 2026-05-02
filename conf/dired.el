b9f76782603c4a16968633cc0c995fb723e9450b
Dave Benjamin <dave@teamaud.io>
2026-05-02 04:43:03 -0700

    Swap "o" and "C-o" bindings in dired

--- conf/dired.el
+++ conf/dired.el
@@ -1,4 +1,6 @@
 ;;; dired.el --- Dired mode customizations
 
 (with-eval-after-load 'dired
+  (define-key dired-mode-map "\C-o" 'dired-find-file-other-window)
+  (define-key dired-mode-map "o" 'dired-display-file)
   (define-key dired-mode-map (kbd ".") 'dired-up-directory))
