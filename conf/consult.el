143364981414dfa3fe6e57bf215d0abf89fbfb7e
Dave Benjamin <dave@teamaud.io>
2026-06-27 08:52:54 -0700

    Add consult-goto-line and reorg

--- conf/consult.el
+++ conf/consult.el
@@ -13,9 +13,17 @@
 (use-package consult
   :ensure t
   :bind (("C-x b" . consult-buffer)
-         ("C-c g" . consult-ripgrep))
+         ("C-c g" . consult-ripgrep)
+         ("M-g" . consult-goto-line))
   :config
   (consult-customize
    consult-buffer
    consult-project-buffer
    :preview-key nil))
+
+;; During isearch, C-o exits and opens consult-line with the current search term
+(define-key isearch-mode-map (kbd "C-o") (lambda ()
+  (interactive)
+  (let ((query isearch-string))
+    (isearch-exit)
+    (consult-line query))))
--- conf/keys.el
+++ conf/keys.el
@@ -39,11 +39,3 @@
 ;; Line-oriented commands
 (global-set-key (kbd "<home>") 'move-beginning-of-line)
 (global-set-key (kbd "<end>") 'move-end-of-line)
-(global-set-key (kbd "M-g") 'goto-line)
-
-;; During isearch, C-o exits and opens consult-line with the current search term
-(define-key isearch-mode-map (kbd "C-o") (lambda ()
-  (interactive)
-  (let ((query isearch-string))
-    (isearch-exit)
-    (consult-line query))))
